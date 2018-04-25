41e4e5e7a477d8134c119c5493fda27429d462b7
神奇辉 <shenqihui0920@gmail.com>
2018-04-25 10:03:54 +0800

    首页增加 icon 例子。

--- src/components_app/home/index.js
+++ src/components_app/home/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import { connect } from 'dva';
+import { Icon } from 'antd';
+
 import styles from './index.less';
+import Svg from '../../components_atom/svg';
 
 class Component extends React.Component {
+  constructor(props) {
+    super(props);
+    debugAdd('home', this);
+    this.iconArr = [
+      {
+        url: require('../../sprites/svg/search.svg'),
+        type: 'search',
+      },
+      {
+        url: require('../../sprites/svg/mail.svg'),
+        type: 'mail',
+      },
+    ];
+  }
+
   componentDidMount = () => {
     this.props.dispatch({
       type: 'breadcrumb/current',
@@ -18,6 +36,21 @@ class Component extends React.Component {
           <li>这里是管理系统。</li>
           <li>主要管理智慧教育方面的信息。</li>
         </ul>
+
+        <div className={styles.iconList}>
+          {
+            this.iconArr.map((icon) => {
+              return (<Icon key={icon.type} type={icon.type} style={{ fontSize: '40px', color: '#000000' }} />);
+            })
+          }
+          <br />
+          {
+            this.iconArr.map((icon) => {
+              return (<Svg key={icon.type} link={icon.url} style={{ fontSize: '40px', color: '#000000' }} />);
+            })
+          }
+        </div>
+
       </div>
     );
   }
