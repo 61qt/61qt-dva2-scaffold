f827dc94713985398c957d3fefca46376e901400
神奇辉 <shenqihui0920@gmail.com>
2018-04-25 11:53:03 +0800

    add menu config

---
+++ src/modules/cas/menu_config.js
@@ -0,0 +1,32 @@
+// 以下所有的那些注释，都是未完成的未开放的功能，不要随便进行删除。
+
+export default [
+  {
+    name: '首页',
+    key: '/',
+    icon: 'home',
+    url: '',
+    // resourceKey: '首页',
+  },
+  {
+    name: '登录',
+    key: '/cas/',
+    icon: 'home',
+    url: '',
+    // resourceKey: '首页',
+  },
+  {
+    name: '忘记密码',
+    key: '/cas/forget',
+    icon: 'home',
+    url: '',
+    // resourceKey: '首页',
+  },
+  {
+    name: '注册',
+    key: '/cas/reg',
+    icon: 'home',
+    url: '',
+    // resourceKey: '首页',
+  },
+];
