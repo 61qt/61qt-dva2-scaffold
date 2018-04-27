d9f3eaa051caee7cc52c8155b2bb8295a18797e3
神奇辉 <shenqihui0920@gmail.com>
2018-04-27 15:54:05 +0800

    src/modules/app/menu_config.js 完善。

--- src/modules/app/menu_config.js
+++ src/modules/app/menu_config.js
@@ -2,26 +2,6 @@
 
 export default [
   {
-    name: '基础数据管理',
-    key: 'base',
-    icon: 'base',
-    // resourceKey: '基础数据管理',
-    child: [
-      {
-        name: '组织信息',
-        key: 'org',
-        url: 'org',
-        // resourceKey: '组织信息',
-      },
-      {
-        name: '学年信息',
-        key: 'school_year',
-        url: 'school_year',
-        // resourceKey: '学年信息',
-      },
-    ],
-  },
-  {
     name: '管理员管理',
     key: 'admin',
     icon: 'admin',
@@ -65,29 +45,37 @@ export default [
         url: 'grade',
         // resourceKey: '班级管理',
       },
+    ],
+  },
+  {
+    name: '教师管理',
+    key: 'teacher',
+    url: 'teacher',
+    // resourceKey: '教师管理',
+  },
+  {
+    name: '学生管理',
+    key: 'student',
+    url: 'student',
+    // resourceKey: '学生管理',
+  },
+  {
+    name: '家长管理',
+    key: 'parent',
+    url: 'parent',
+    // resourceKey: '家长管理',
+  },
+  {
+    name: '用户管理',
+    key: 'user_manage',
+    url: 'user_manage',
+    // resourceKey: '用户管理',
+    child: [
       {
-        name: '教师管理',
-        key: 'teacher',
-        url: 'teacher',
-        // resourceKey: '教师管理',
-      },
-      {
-        name: '学生管理',
-        key: 'student',
-        url: 'student',
-        // resourceKey: '学生管理',
-      },
-      {
-        name: '家长管理',
-        key: 'parent',
-        url: 'parent',
-        // resourceKey: '家长管理',
-      },
-      {
-        name: '用户管理',
+        name: '用户列表',
         key: 'user',
         url: 'user',
-        // resourceKey: '用户管理',
+        // resourceKey: '用户列表',
       },
       {
         name: '用户组管理',
@@ -104,36 +92,56 @@ export default [
     ],
   },
   {
-    name: '第三方应用认证',
-    key: 'thirdparty',
-    icon: 'thirdparty',
-    // resourceKey: '第三方应用认证',
-    child: [
-      {
-        name: '第三方授权',
-        key: 'thirdparty_auth',
-        url: 'thirdparty_auth',
-        // resourceKey: '第三方授权',
-      },
-      {
-        name: '认证接口',
-        key: 'thirdparty_auth_api',
-        url: 'thirdparty_auth_api',
-        // resourceKey: '认证接口',
-      },
-      {
-        name: '认证日志',
-        key: 'thirdparty_auth_log',
-        url: 'thirdparty_auth_log',
-        // resourceKey: '认证日志',
-      },
-    ],
-  },
-  {
     name: '文章管理',
     key: 'news',
     icon: 'news',
     url: 'news',
     // resourceKey: '文章管理',
+  },
+  {
+    name: '基础数据管理',
+    key: 'base',
+    icon: 'base',
+    resourceKey: '基础数据管理',
+    child: [
+      {
+        name: '组织信息',
+        key: 'org',
+        url: 'org',
+        resourceKey: '组织信息',
+      },
+      {
+        name: '学年信息',
+        key: 'school_year',
+        url: 'school_year',
+        resourceKey: '学年信息',
+      },
+    ],
+  },
+  {
+    name: '第三方应用认证',
+    key: 'thirdparty',
+    icon: 'thirdparty',
+    resourceKey: '第三方应用认证',
+    child: [
+      {
+        name: '第三方授权',
+        key: 'thirdparty_auth',
+        url: 'thirdparty_auth',
+        resourceKey: '第三方授权',
+      },
+      {
+        name: '认证接口',
+        key: 'thirdparty_auth_api',
+        url: 'thirdparty_auth_api',
+        resourceKey: '认证接口',
+      },
+      {
+        name: '认证日志',
+        key: 'thirdparty_auth_log',
+        url: 'thirdparty_auth_log',
+        resourceKey: '认证日志',
+      },
+    ],
   },
 ];
