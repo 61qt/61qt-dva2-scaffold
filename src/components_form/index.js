4e9ad33ac99fdcdf893c1d896f022361ac931a60
神奇辉 <shenqihui0920@gmail.com>
2018-05-03 11:09:29 +0800

    add ForeignSelectGraphql

--- src/components_form/index.js
+++ src/components_form/index.js
@@ -1,5 +1,6 @@
 import DateRange from './date_range';
 import ForeignSelect from './foreign_select';
+import ForeignSelectGraphql from './foreign_select_graphql';
 import HtmlBraftEditor from './html_braft_editor';
 // import HtmlEditor from './html_editor';
 import ImageUploader from './image_uploader';
@@ -10,6 +11,7 @@ import WeekRange from './week_range';
 export default {
   DateRange,
   ForeignSelect,
+  ForeignSelectGraphql,
   HtmlBraftEditor,
   // HtmlEditor,
   ImageUploader,
