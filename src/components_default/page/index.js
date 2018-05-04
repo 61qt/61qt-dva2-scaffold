27abcf53c623784d1b50d421265c403ae745d007
神奇辉 <shenqihui0920@gmail.com>
2018-05-04 21:15:05 +0800

    src/components_default/page 增加，暴露配置接口。

--- src/components_default/page/index.js
+++ src/components_default/page/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { connect } from 'dva';
-import { Pagination, Button } from 'antd';
+import _ from 'lodash';
+import { message, Pagination, Button } from 'antd';
 import { NavLink } from 'dva/router';
 import styles from './index.less';
 import Filters from '../../filters';
@@ -8,31 +8,136 @@ import Download from '../../components_atom/download';
 import Upload from '../../components_atom/upload';
 import Access from '../../components_atom/access';
 import Table from '../../components_atom/table';
+import Services from '../../services';
+import SearchForm, {
+  getFilter,
+} from '../../components_atom/search_form';
+import FilterTree from '../../components_atom/filter_tree';
+import PageLayout from '../../components_atom/page-layout';
 
-@connect(() => {
-  return {};
-})
 export default class Component extends React.Component {
   constructor(props) {
     super(props);
-    window.console.log('暂时用不了');
     debugAdd('page', this);
     this.state = {
-      model: 'student',
+      // 搜索框的那个每一列的 col 长度，参考 grid
+      searchCol: 12,
+      // filter tree 的展开深度
+      filterTreeDeep: 99999999,
+      // 当前页面的展示的表(service 或者是 schema)的名称。
+      model: 'admin_city',
+      // 当前页面的展示的表(service 或者是 schema)的中文可读名称。
       modeLabel: '管理员',
-      columns: [],
-      modelState: {},
-      loading: false,
+      // 当前初始化就要使用到的查询条件。
+      defaultSearchValue: {},
     };
   }
 
-  componentDidMount = () => {}
+  componentDidMount = () => {
+    if (__DEV__) {
+      window.console.log('[componentDidMount] 如果需要配置导航条，需要在子类重新定义该方法');
+    }
+  }
 
   onUploaded = (info) => {
     if (__DEV__) {
       window.console.log('info', info);
     }
     this.resetPage();
+  }
+
+  onSiderSelect = (selected) => {
+    this.handleSubmit({
+      siderValues: {
+        city_id: selected[0] || selected,
+      },
+    });
+  }
+
+  onSiderCheck = (selected) => {
+    window.console.log('onCheck', selected);
+  }
+
+  getTableColumns = () => {
+    if (__DEV__) {
+      window.console.log('[getTableColumns] 如果需要配置页面 table，需要在子类重新定义该方法');
+    }
+    return null;
+  }
+
+  getTable = () => {
+    const columns = this.getTableColumns();
+    if (columns && columns.length) {
+      return (<div>
+        <Table
+          data-bak-size={768 > window.innerWidth ? 'small' : 'default'}
+          size="small"
+          bordered
+          columns={columns}
+          dataSource={this.props.pageState.list}
+          loading={this.props.loading}
+          scroll={{ x: columns.reduce((a, b) => (a.width || a.minWidth || a || 0) + (b.width || b.minWidth || 0), 0), y: 300 > window.innerHeight - 310 ? 300 : window.innerHeight - 310 }}
+          rowKey={record => record.id}
+          pagination={false}
+          title={this.title}
+          footer={this.footer}
+        />
+      </div>);
+    }
+    return null;
+  }
+
+  getSearchColumn = () => {
+    if (__DEV__) {
+      window.console.log('[getSearchColumn] 如果需要配置搜索框，需要在子类重新定义该方法');
+    }
+    return null;
+  }
+
+  getSearchForm = () => {
+    let showCount = 3;
+    if (12 < this.state.searchCol * 1) {
+      showCount = 1;
+    }
+    else if (12 === this.state.searchCol * 1) {
+      showCount = 2;
+    }
+    else if (8 === this.state.searchCol * 1) {
+      showCount = 3;
+    }
+
+    if (this.getSearchColumn) {
+      return (<SearchForm
+        handleSubmit={this.handleSubmit}
+        form={this.props.form}
+        defaultExpand={_.get(this.props.pageState, 'listState.expand') || false}
+        defaultSearchValues={_.get(this.props.pageState, 'listState.searchValues') || {}}
+        searchColumn={this.getSearchColumn()}
+        showCount={showCount} />);
+    }
+    return null;
+  }
+
+  getSiderTree = () => {
+    if (__DEV__) {
+      window.console.log('[getSiderTree] 如果需要配置左侧搜索树，需要在子类重新定义该方法');
+    }
+    return null;
+  }
+
+  getSider = () => {
+    if (this.getSiderTree()) {
+      return (<FilterTree
+        tree={this.getSiderTree()}
+        deep={this.state.filterTreeDeep}
+        onSelect={this.onSiderSelect}
+        onCheck={this.onSiderCheck}
+        checkable={false}
+        multiple={false}
+        defaultValue={_.get(this.props, 'pageState.listState.sideSelected') || []}
+      />);
+    }
+    return null;
   }
 
   resetPage = () => {
@@ -43,44 +148,83 @@ export default class Component extends React.Component {
     });
   }
 
-  pageChangeHandler = (page = this.state.modelState.page) => {
+  pageChangeHandler = (page = this.props.pageState.page) => {
     const { dispatch } = this.props;
     dispatch({
       type: `${this.state.model}/list`,
-      payload: { page, filter: this.state.modelState.listState.filter },
+      payload: { page, filter: this.props.pageState.listState.filter },
     });
   }
 
-  handleSubmit = ({ filter, values, expand, loadOldPage }) => {
+  handleRemove = ({ record }) => {
+    const { dispatch } = this.props;
+    dispatch({
+      type: `${this.state.model}/remove`,
+      payload: {
+        id: record.id,
+      },
+    }).then(() => {
+      message.success('删除成功');
+      this.pageChangeHandler();
+    }).catch(() => {
+      message.success('删除成功');
+      this.pageChangeHandler();
+    });
+  }
+
+  handleStatusChange = ({ record, values }) => {
+    Services[this.state.model].graphqlPatchUpdate('changeStatus', record.id, values).then(() => {
+      message.success('设置成功');
+      this.pageChangeHandler();
+    }).catch(() => {
+      message.success('设置成功');
+      this.pageChangeHandler();
+    });
+  }
+
+  handleSubmit = ({
+    searchValues = _.get(this.props.pageState, 'listState.searchValues') || {},
+    siderValues = _.get(this.props.pageState, 'listState.siderValues') || {},
+    expand = _.get(this.props.pageState, 'listState.expand') || false,
+    loadOldPage = false,
+  }) => {
+    const filter = getFilter({
+      ...searchValues,
+      ...siderValues,
+      ...this.state.defaultSearchValue,
+    });
+
     const { dispatch } = this.props;
     dispatch({
       type: `${this.state.model}/listState`,
-      payload: { filter, searchValues: values, expand },
+      payload: { filter, siderValues, searchValues, expand },
     });
     dispatch({
       type: `${this.state.model}/list`,
-      payload: { page: loadOldPage ? this.state.modelState.page : 1, filter },
+      payload: { page: loadOldPage ? this.props.pageState.page : 1, filter },
     });
   }
 
   title = () => {
+    const { pageState = {} } = this.props;
+
     return (
       <div className="clearfix">
         <h3 className="table-title">
           {this.state.modeLabel}列表
-          { this.state.modelState.total ? <small>（共{this.state.modelState.total}条）</small> : null }
+          { pageState.total ? <small>（共{pageState.total}条）</small> : null }
         </h3>
 
         <div className="table-title-action">
-          <Access auth={`${this.state.model}.store`}>
+          <Access data-bak-auth={`${this.state.model}.store`}>
             <Upload onUploaded={this.onUploaded} size="small" path={`${this.state.model}?upload`}>批量导入</Upload>
           </Access>
-          <Access auth={`${this.state.model}.export`}>
-            <Download confirm="true" selectRow={this.columns} size="small" path={`${this.state.model}/export`} query={{ filter: this.state.modelState.listState.filter }}>批量导出</Download>
+          <Access data-bak-auth={`${this.state.model}.export`}>
+            <Download confirm="true" selectRow={this.columns} size="small" path={`${this.state.model}/export`} query={{ filter: _.get(pageState, 'listState.filter') }}>批量导出</Download>
           </Access>
-          <Access auth={`${this.state.model}.store`}>
-            <NavLink to={Filters.path(`${this.state.model}_add`, {})} activeClassName="link-active">
-              <Button size="small" type="primary" ghost>新增{this.state.modelState}</Button>
+          <Access data-bak-auth={`${this.state.model}.store`}>
+            <NavLink to={Filters.path(`${this.state.model}_add`, {})}>
+              <Button size="small" type="primary" ghost>新增</Button>
             </NavLink>
           </Access>
           <Download link="true" size="small" path={`${this.state.model}/export/template`}>下载模板</Download>
@@ -91,14 +235,15 @@ export default class Component extends React.Component {
   }
 
   footer = () => {
+    const { pageState } = this.props;
     return (
       <div className="clearfix">
-        <div className="ant-table-pagination-info">当前显示{this.state.modelState.start} - {this.state.modelState.end}条记录，共 {this.state.modelState.total} 条数据</div>
+        <div className="ant-table-pagination-info">当前显示{pageState.start} - {pageState.end}条记录，共 {pageState.total} 条数据</div>
         <Pagination
           className="ant-table-pagination ant-table-pagination-hide-last"
-          total={this.state.modelState.total}
-          current={this.state.modelState.page}
-          pageSize={this.state.modelState.pageSize}
+          total={pageState.total}
+          current={pageState.page}
+          pageSize={pageState.pageSize}
           showQuickJumper={false}
           size="small"
           onChange={this.pageChangeHandler}
@@ -107,28 +252,12 @@ export default class Component extends React.Component {
     );
   }
 
-
   render() {
-    const SearchForm = this.state.SearchForm;
-    return (
+    return (<PageLayout Sider={this.getSider()}>
       <div className={`${styles.normal}`}>
-        <SearchForm handleSubmit={this.handleSubmit} />
-        <div>
-          <Table
-            data-bak-size={768 > window.innerWidth ? 'small' : 'default'}
-            size="small"
-            bordered
-            columns={this.state.columns}
-            dataSource={this.state.modelState.list}
-            loading={this.state.loading}
-            scroll={{ x: this.state.columns.reduce((a, b) => (a.width || a.minWidth || a || 0) + (b.width || b.minWidth || 0), 0), y: 300 > window.innerHeight - 310 ? 300 : window.innerHeight - 310 }}
-            rowKey={record => record.id}
-            pagination={false}
-            title={this.title}
-            footer={this.footer}
-          />
-        </div>
+        { this.getSearchForm() }
+        { this.getTable() }
       </div>
-    );
+    </PageLayout>);
   }
 }
