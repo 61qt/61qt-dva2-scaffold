c9a62ca484e70b8af62358dae0a1e1e0d8c0ec0a
zyx <mygmzyx@gdzhsng.com>
2018-05-05 10:50:01 +0800

    src/components_default/page_list/index.js 增加 constructorExtend 方法

--- src/components_default/page_list/index.js
+++ src/components_default/page_list/index.js
@@ -32,6 +32,8 @@ export default class Component extends React.Component {
       // 当前初始化就要使用到的查询条件。
       defaultSearchValue: {},
     };
+
+    this.constructorExtend(props);
   }
 
   componentDidMount = () => {
@@ -144,6 +146,12 @@ export default class Component extends React.Component {
       />);
     }
     return null;
+  }
+
+  constructorExtend = () => {
+    if (__DEV__) {
+      window.console.log('[constructorExtend] 需要在子类重新定义该方法，接收 props 参数');
+    }
   }
 
   resetPage = () => {
