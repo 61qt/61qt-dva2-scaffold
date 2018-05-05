cc56de4675d71cdbf3bfcc14972865f250bf696f
zyx <mygmzyx@gdzhsng.com>
2018-05-05 11:24:01 +0800

    src/components_app/student/index.js 使用默认的 PageList 组件

--- src/components_app/student/index.js
+++ src/components_app/student/index.js
@@ -1,35 +1,160 @@
 import React from 'react';
-import _ from 'lodash';
+// import _ from 'lodash';
 import { connect } from 'dva';
-import { Pagination, Button } from 'antd';
+import { Col, Form, Input, Select } from 'antd';
 import { NavLink } from 'dva/router';
 import styles from './index.less';
 import Filters from '../../filters';
 // import EditModal from './modal';
-import SearchForm from './search_form';
-import Download from '../../components_atom/download';
-import Upload from '../../components_atom/upload';
 import Access from '../../components_atom/access';
-import Table from '../../components_atom/table';
-import PageLayout from '../../components_atom/page-layout';
-import {
-  getFilter,
-} from '../../components_default/search_form';
+import PageList from '../../components_default/page_list';
+import ComponentsForm from '../../components_form';
+import { searchFormItemLayout } from '../../components_atom/search_form';
 
+@Form.create()
 @connect((state) => {
   return {
     loading: !!state.loading.models.student,
-    studentState: state.student,
+    pageState: state.student,
   };
 })
-export default class Component extends React.Component {
+export default class Component extends PageList {
   constructor(props) {
     super(props);
     debugAdd('student', this);
-    this.state = {
-      defaultSearchValue: {},
-    };
-    this.columns = [
+    Object.assign(this.state, {
+      searchCol: 12,
+      filterTreeDeep: 1,
+      model: 'student',
+      modeLabel: '学生管理',
+      defaultSearchValue: {
+      },
+    });
+  }
+
+  componentDidMount = () => {
+    const { dispatch } = this.props;
+    dispatch({
+      type: 'breadcrumb/current',
+      payload: [
+        {
+          name: '学生管理',
+          url: Filters.path('student', {}),
+        },
+      ],
+    });
+  }
+
+  getSearchColumn = () => {
+    const getFieldDecorator = this.props.form.getFieldDecorator;
+    const children = [];
+
+    children.push((
+      <Col span={this.state.searchCol} key="name">
+        <Form.Item {...searchFormItemLayout} label="姓名">
+          {getFieldDecorator('name')(<Input size="small" placeholder="姓名搜索" />)}
+        </Form.Item>
+      </Col>
+    ));
+
+    children.push((
+      <Col span={this.state.searchCol} key="id">
+        <Form.Item {...searchFormItemLayout} label="学号">
+          {getFieldDecorator('id')(<Input size="small" placeholder="学号搜索" />)}
+        </Form.Item>
+      </Col>
+    ));
+
+    children.push((
+      <Col span={this.state.searchCol} key="sng_admin_id">
+        <Form.Item {...searchFormItemLayout} label="课程顾问">
+          {
+            getFieldDecorator('sng_admin_id')(<ComponentsForm.ForeignSelect
+              size="small"
+              placeholder="课程顾问"
+              url="admin"
+              search={{ format: 'filter', name: 'name', method: 'like' }}
+              allowClear
+              numberFormat
+            />)
+          }
+        </Form.Item>
+      </Col>
+    ));
+
+    children.push((
+      <Col span={this.state.searchCol} key="gender">
+        <Form.Item {...searchFormItemLayout} label="选择性别">
+          {
+            getFieldDecorator('gender')(<Select size="small" allowClear placeholder="选择">
+              {Filters.dict(['student', 'gender']).map((elem) => {
+                return (
+                  <Select.Option
+                    value={`${elem.value}`}
+                    key={`gender_${elem.value}`}
+                  >
+                    {elem.label}
+                  </Select.Option>
+                );
+              })}
+            </Select>)}
+        </Form.Item>
+      </Col>
+    ));
+
+    children.push((
+      <Col span={this.state.searchCol} key="primary_name">
+        <Form.Item {...searchFormItemLayout} label="主要联系人姓名">
+          {getFieldDecorator('primary_name')(<Input size="small" placeholder="主要联系人姓名" />)}
+        </Form.Item>
+      </Col>
+    ));
+
+    children.push((
+      <Col span={this.state.searchCol} key="primary_phone">
+        <Form.Item {...searchFormItemLayout} label="主要联系人电话">
+          {getFieldDecorator('primary_phone')(<Input size="small" placeholder="主要联系人电话" />)}
+        </Form.Item>
+      </Col>
+    ));
+
+    children.push((
+      <Col span={this.state.searchCol} key="secondary_name">
+        <Form.Item {...searchFormItemLayout} label="次要联系人姓名">
+          {getFieldDecorator('secondary_name')(<Input size="small" placeholder="次要联系人姓名" />)}
+        </Form.Item>
+      </Col>
+    ));
+
+    children.push((
+      <Col span={this.state.searchCol} key="secondary_phone">
+        <Form.Item {...searchFormItemLayout} label="次要联系人手机">
+          {getFieldDecorator('secondary_phone')(<Input size="small" placeholder="次要联系人手机" />)}
+        </Form.Item>
+      </Col>
+    ));
+
+    children.push((
+      <Col span={this.state.searchCol} key="phone">
+        <Form.Item {...searchFormItemLayout} label="学生手机">
+          {getFieldDecorator('phone')(<Input size="small" placeholder="学生手机" />)}
+        </Form.Item>
+      </Col>
+    ));
+
+    children.push((
+      <Col span={this.state.searchCol} key="start_end_time">
+        <Form.Item {...searchFormItemLayout} label="出生日期">
+          {getFieldDecorator('start_end_time')(<ComponentsForm.DateRange size="small" format="YYYY-MM-DD" />)}
+        </Form.Item>
+      </Col>
+    ));
+
+    return children;
+  }
+
+  getTableColumns = () => {
+    const columns = [
       {
         title: '学号',
         dataIndex: 'id',
@@ -113,132 +238,7 @@ export default class Component extends React.Component {
         },
       },
     ];
-  }
 
-  componentDidMount = () => {
-    const { dispatch } = this.props;
-    dispatch({
-      type: 'breadcrumb/current',
-      payload: [
-        {
-          name: '学生管理',
-          url: Filters.path('student', {}),
-        },
-      ],
-    });
-  }
-
-  onUploaded = (info) => {
-    if (__DEV__) {
-      window.console.log('info', info);
-    }
-    this.resetPage();
-  }
-
-  resetPage = () => {
-    this.props.dispatch({
-      type: 'student/reset',
-    }).then(() => {
-      this.props.history.push(Filters.path('loading', {}));
-    });
-  }
-
-  pageChangeHandler = (page = this.props.studentState.page) => {
-    const { dispatch } = this.props;
-    dispatch({
-      type: 'student/list',
-      payload: { page, filter: this.props.studentState.listState.filter },
-    });
-  }
-
-  handleSubmit = ({
-    searchValues = _.get(this.props.studentState, 'listState.searchValues') || {},
-    siderValues = _.get(this.props.studentState, 'listState.siderValues') || {},
-    expand = _.get(this.props.studentState, 'listState.expand') || false,
-    loadOldPage = false,
-  }) => {
-    const filter = getFilter({
-      ...searchValues,
-      ...siderValues,
-      ...this.state.defaultSearchValue,
-    });
-
-    const { dispatch } = this.props;
-    dispatch({
-      type: 'student/listState',
-      payload: { filter, siderValues, searchValues, expand },
-    });
-    dispatch({
-      type: 'student/list',
-      payload: { page: loadOldPage ? this.props.studentState.page : 1, filter },
-    });
-  }
-
-  title = () => {
-    const { studentState } = this.props;
-    return (
-      <div className="clearfix">
-        <h3 className="table-title">
-          学生列表
-          { studentState.total ? <small>（共{studentState.total}条）</small> : null }
-        </h3>
-
-        <div className="table-title-action">
-          <Access auth="student.store">
-            <Upload onUploaded={this.onUploaded} size="small" path="student?upload">批量导入</Upload>
-          </Access>
-          <Access auth="student.export">
-            <Download confirm="true" selectRow={this.columns} size="small" path="student/export" query={{ filter: studentState.listState.filter }}>批量导出</Download>
-          </Access>
-          <Access auth="student.store">
-            <NavLink to={Filters.path('student_add', {})} activeClassName="link-active">
-              <Button size="small" type="primary" ghost>新增学生</Button>
-            </NavLink>
-          </Access>
-          <Download link="true" size="small" path="student/export/template">下载模板</Download>
-        </div>
-      </div>
-    );
-  }
-
-  footer = () => {
-    const { studentState } = this.props;
-    return (
-      <div className="clearfix">
-        <div className="ant-table-pagination-info">当前显示{studentState.start} - {studentState.end}条记录，共 {studentState.total} 条数据</div>
-        <Pagination
-          className="ant-table-pagination ant-table-pagination-hide-last"
-          total={studentState.total}
-          current={studentState.page}
-          pageSize={studentState.pageSize}
-          showQuickJumper={false}
-          size="small"
-          onChange={this.pageChangeHandler}
-        />
-      </div>
-    );
-  }
-
-  render() {
-    return (
-      <PageLayout>
-        <SearchForm handleSubmit={this.handleSubmit} />
-        <div>
-          <Table
-            data-bak-size={768 > window.innerWidth ? 'small' : 'default'}
-            size="small"
-            bordered
-            columns={this.columns}
-            dataSource={this.props.studentState.list}
-            loading={this.props.loading}
-            scroll={{ x: this.columns.reduce((a, b) => (a.width || a.minWidth || a || 0) + (b.width || b.minWidth || 0), 0), y: 300 > window.innerHeight - 310 ? 300 : window.innerHeight - 310 }}
-            rowKey={record => record.id}
-            pagination={false}
-            title={this.title}
-            footer={this.footer}
-          />
-        </div>
-      </PageLayout>
-    );
+    return columns;
   }
 }
